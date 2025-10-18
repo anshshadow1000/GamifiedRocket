1335e1cf7f39d3281ee27d54b6c6c712a39c4e84
anshshadow1000 <ansh.shadow1000@gmail.com>
2025-10-18 12:55:47 -0400

    Added terrain, gravity and movement of rocket

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -5,15 +5,33 @@ public class Lander : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Update is called once per frame
-    private void Update()
+    
+    private Rigidbody2D landerRigidbody2D;
+
+
+
+    private void Awake() {
+        landerRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
     {
         if (Keyboard.current.upArrowKey.isPressed)
         {
-            Debug.Log("Up");
+            float force = 700f;
+            landerRigidbody2D.AddForce(force * transform.up * Time.deltaTime);
         }
-        if (Keyboard.current.downArrowKey.isPressed)
+        if (Keyboard.current.leftArrowKey.isPressed)
         {
-            Debug.Log("Down");
+            float turnspeed = +100f;
+            landerRigidbody2D.AddTorque(turnspeed * Time.deltaTime);
+        }
+        if (Keyboard.current.rightArrowKey.isPressed)
+        {
+            float turnspeed = -100f;
+            landerRigidbody2D.AddTorque(turnspeed * Time.deltaTime);
         }
     }
 }
+
