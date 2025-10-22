71cb7b60fed7b03e3c7fb64609825a54b15d9868
anshshadow1000 <ansh.shadow1000@gmail.com>
2025-10-21 20:08:46 -0400

    added arrow's to show direction and rounding of speed, fuel and time

--- Assets/Scripts/StatsUI.cs
+++ Assets/Scripts/StatsUI.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 public class StatsUI : MonoBehaviour
 {
 
-    [SerializeField]private TextMeshProUGUI statsTextMesh;
+    [SerializeField] private TextMeshProUGUI statsTextMesh;
+    [SerializeField] private GameObject speedUpArrowGameObject;
+    [SerializeField] private GameObject speedDownArrowGameObject;
+    [SerializeField] private GameObject speedRightArrowGameObject;
+    [SerializeField] private GameObject speedLeftArrowGameObject;
+
 
     private void Update()
     {
@@ -13,10 +18,14 @@ public class StatsUI : MonoBehaviour
 
     private void UpdateStatsTextMesh()
     {
+        speedDownArrowGameObject.SetActive(Lander.Instance.GetSpeedY() < 0);
+        speedUpArrowGameObject.SetActive(Lander.Instance.GetSpeedY() >= 0);
+        speedLeftArrowGameObject.SetActive(Lander.Instance.GetSpeedX() < 0);
+        speedRightArrowGameObject.SetActive(Lander.Instance.GetSpeedX() >= 0);
         statsTextMesh.text = GameManager.Instance.GetScore() + "\n" +
-        GameManager.Instance.GetTime() + "\n" + 
-        Lander.Instance.GetSpeedX() + "\n" +
-        Lander.Instance.GetSpeedY() + "\n" +
+        Mathf.Round(GameManager.Instance.GetTime()) + "\n" +
+        Mathf.Round(Mathf.Abs(Lander.Instance.GetSpeedX() * 10f)) + "\n" +
+        Mathf.Round(Mathf.Abs(Lander.Instance.GetSpeedY() * 10f)) + "\n" +
         Lander.Instance.GetFuelAmount() + "\n";
     }
 
