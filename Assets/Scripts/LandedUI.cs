b123e1b43de37b8a4e7f4ef255c505ecd42779f4
anshshadow1000 <ansh.shadow1000@gmail.com>
2025-10-31 21:20:58 -0400

    Added restart button

--- Assets/Scripts/LandedUI.cs
+++ Assets/Scripts/LandedUI.cs
@@ -1,5 +1,8 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class LandedUI : MonoBehaviour
 {
@@ -7,6 +10,17 @@ public class LandedUI : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI titleTextMesh;
     [SerializeField] private TextMeshProUGUI statsTextMesh;
+    [SerializeField] private Button nextButton;
+
+
+    private void Awake()
+    {
+        nextButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(0);
+        });
+    }
+
 
     private void Start()
     {
